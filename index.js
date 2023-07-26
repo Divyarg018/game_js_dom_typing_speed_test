2269a08da231e1f2f6dbbd6dfeaa2dc476aac6bb
Divya R <kwrdivyaraj100@gmail.com>
2023-07-26 15:46:01 +0530

    working with const startTyping and endTyping in css file

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const setOfWords = ["Do you love the typing speed test game?"];
+const setOfWords = ["Do you love the typing speed test game?",
+    "Start typing the text to check the typing speed.",
+    "You have done a great job...!"];
 const msg = document.getElementById('showSentence');
 const typeWords = document.getElementById('textarea');
 const btn = document.getElementById('btn');
 const score = document.getElementById('score');
-let startTime, endTime;
+let startTime, endTime, totalTimeTaken;
+
+const startTyping = () => {
+    let randomNumber = Math.floor(Math.random() * setOfWords.length);
+    console.log(randomNumber);
+    msg.innerHTML = setOfWords[randomNumber];
+
+    let date = new Date();
+    startTime = date.getTime();
+
+    btn.innerText = "Done";
+}
+
+const endTyping = () => {
+    btn.innerText = "Start";
+
+    let date = new Date();
+    endTime = date.getTime();
+
+    totalTimeTaken = (endTime -startTime) / 1000;
+
+    calculateTypingSpeed(totalTimeTaken);
+
+    msg.innerHTML = "";
+    typeWords.value = "";
+}
 
 btn.addEventListener('click', () => {
     switch (btn.innerText.toLowerCase()) {
@@ -14,7 +41,7 @@ btn.addEventListener('click', () => {
 
         case "done":
             typeWords.setAttribute('disabled', 'true');
-            endTypingTest();
+            endTyping();
             break;
     }
 })
